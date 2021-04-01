b42288a68134fdbcfa3ee6e5a02bbb8658bbf312
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-01 07:01:53 -0400

    Match workers to number of CPUs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
@@ -25,8 +26,10 @@ app.get('/timer', (req, res) => {
 console.log('Running server.js...');
 if (cluster.isMaster) {
   console.log('Master has been started...');
-  cluster.fork();
-  cluster.fork();
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
 } else {
   console.log('Worker process started.');
   app.listen(3000);
