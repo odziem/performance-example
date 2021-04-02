12524416c0e5bc816ed9a724b3100b6694c7bae9
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-04-02 06:42:14 -0400

    Add PM2 and .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
 
 const app = express();
 
@@ -12,9 +10,6 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  // JSON.stringify({}) => "{}"
-  // JSON.parse("{}") => {}
-  // [5,1,2,3,4].sort()
   res.send(`Performance example: ${process.pid}`);
 });
 
@@ -24,13 +19,5 @@ app.get('/timer', (req, res) => {
 });
 
 console.log('Running server.js...');
-if (cluster.isMaster) {
-  console.log('Master has been started...');
-  const NUM_WORKERS = os.cpus().length;
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log('Worker process started.');
-  app.listen(3000);
-}
+console.log('Worker process started.');
+app.listen(3000);
